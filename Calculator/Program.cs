129e72fc9516acfedc33b3f912de84a4546c3f03
adarsh-1998-proj <64652298+adarsh-1998-proj@users.noreply.github.com>
2023-12-23 23:33:43 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Input the first number:");
+        string firstNumber = Console.ReadLine();
+        int number = int.Parse(firstNumber);
+        Console.WriteLine("Input the second number:");
+        string secondNumber = Console.ReadLine();
+        int number2 = int.Parse(firstNumber);
+        Console.WriteLine("What do you want to do with those numbers?");
+        Console.WriteLine("[A]dd");
+        Console.WriteLine("[S]ubtract");
+        Console.WriteLine("[M]ultiply");
+
+
+
+        var userChoise = Console.ReadLine();
+        if (userChoise == "A")
+        {
+            var addition = number + number2;
+            Console.WriteLine(firstNumber + "+" + secondNumber + "=" + addition);
+        }
+        void PrintSelectedOption(string selectedOption)
+        {
+            Console.WriteLine("Selected option: " + selectedOption);
+        }
+    }
+}
---
+++ Todolist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("what do you want to do");
+Console.WriteLine("[S]ee all todo");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[E]xit");
+
+
+//var result = Add(12, 15);
+//Console.WriteLine("12 + 15 :" + result);
+
+var userChoise = Console.ReadLine();
+if (userChoise == "S")
+{
+    PrintSelectedOption("Selected option: see all todos");
+}
+else if (userChoise == "A")
+{
+    PrintSelectedOption("Selected option: Add a todo");
+}
+else if (userChoise == "R")
+{
+    PrintSelectedOption("Selected option: Remove a todo");
+
+}
+else
+{
+    PrintSelectedOption("Selected option: Exit");
+}
+Console.ReadKey();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
+
+
+//int Add(int a, int b)
+//{ return a + b; }
